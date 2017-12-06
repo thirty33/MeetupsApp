9b226c32c0f6add8dbc6710900820018b4fe7dc2
Thirty33 <joelsuarez.1101@gmail.com>
2017-12-06 11:42:51 -0500

    creando carousel con vuetify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,33 @@
 <template>
 	<v-container>
-		<v-layout row wrap class="first">
+		<v-layout row wrap class="first mb-2">
 			<v-flex xs12 sm6 class="text-xs-center text-sm-right">
-				<v-btn large router to="/meetups " class="mt-0 primary">Explore Meetups</v-btn>
+				<v-btn large router to="meetups" class="mt-0 primary">Explore Meetups</v-btn>
 			</v-flex>
 			<v-flex xs12 sm6 class="text-xs-center text-sm-left">
-				<v-btn large router to="/meetup/new" class="mt-0 primary">Organize Meetup</v-btn>
+				<v-btn large router to="meetup/new" class="mt-0 primary">Organize Meetup</v-btn>
 			</v-flex>
 		</v-layout>
+
+		<v-layout row wrap class="mt-2" >
+			<v-flex xs12>
+				<v-carousel>
+					<v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+						<div class="title">
+							{{ meetup.title }}
+						</div>
+					</v-carousel-item>
+				</v-carousel>		
+				
+			</v-flex>
+		</v-layout>
+		
+		<v-layout row wrap class="mt-2">
+			<v-flex xs12 class="text-xs-center">
+				<p>Join our awesome meetups!</p>
+			</v-flex>
+		</v-layout>
+		
 	</v-container>
 </template>
 
@@ -15,15 +35,30 @@
 	export default {
 		data(){
 			return {
-
+				meetups: [
+					{ imageUrl: 'http://www.greycloaktech.com/wp-content/uploads/2015/07/url-small.jpg', id: '8282727', title: 'meetup en cali'},
+					{ imageUrl: 'http://www.greycloaktech.com/wp-content/uploads/2015/07/url-small.jpg', id: '8282727', title: 'meetup en cali'},
+					{ imageUrl: 'http://www.greycloaktech.com/wp-content/uploads/2015/07/url-small.jpg', id: '8282727', title: 'meetup en cali'},
+					{ imageUrl: 'http://www.greycloaktech.com/wp-content/uploads/2015/07/url-small.jpg', id: '8282727', title: 'meetup en cali'},
+					
+				]
 			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
 	.first {
-		position: absolute;
-		top: 73px;
+		/* position: absolute;
+		top: 75px; */
+	}
+
+	.title {
+		position:absolute;
+		bottom: 50px;
+		color: white;
+		background-color: rgba(0,0,0,0.5);
+		font-size: 2em;
+		padding: 20px;
 	}
 </style>
